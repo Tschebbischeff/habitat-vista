9206764c137f6aea90604b3255b22cb9abe29384
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-08-11 17:46:31 +0200

    adds logout button to navigation menu in responsive views

--- habitat-config/glance/assets/js/user_menu.js
+++ habitat-config/glance/assets/js/user_menu.js
@@ -14,4 +14,15 @@ glanceLibRegister(
         `;
         navBar.parentNode.insertBefore(userMenu, navBar.nextSibling);
     }
+    const navBarMobile = document.querySelector("div.mobile-navigation-actions");
+    if (navBarMobile) {
+        const userMenu = document.createElement("div");
+        userMenu.className = "glance-lib-usermenu-mobile";
+        userMenu.innerHTML = `
+            <a href="${dataset.oidcLogoutUrl}">
+                Logout
+            </a>
+        `;
+        navBarMobile.appendChild(userMenu);
+    }
 });
